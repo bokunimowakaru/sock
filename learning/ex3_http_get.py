8308a8c8a2eedc49aa7296973bfb2346777058c8
Wataru KUNINO <git@bokunimo.net>
2025-05-17 00:08:25 +0900

    chmod

--- learning/ex3_http_get.py
+++ learning/ex3_http_get.py
@@ -1,16 +1,29 @@
 #!/usr/bin/env python3
 # coding: utf-8
-# Example 07 IoT連携の基本 HTTP GET
+# Example 03 IoT連携の基本 HTTP GET
 
-import urllib.request                           # HTTP通信ライブラリを組み込む
-import json                                     # JSON変換ライブラリを組み込む
+import socket                                               # ソケットの組み込み
+import json                                                 # JSON変換ライブラリ
 
-url_s = 'https://bokunimo.net/cq/ip/test.json'  # アクセス先を変数url_sへ代入
+http_serv = '210.224.185.41'                                # サーバIPアドレス
+port = 80                                                   # ポート番号を代入
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)    # TCP用ソケット作成
+sock.connect((http_serv, port))                             # TCP接続
 
-res = urllib.request.urlopen(url_s)             # HTTPアクセスを実行
-res_dict = json.loads(res.read().decode())      # 受信データを変数res_dictへ代入
-res.close()                                     # HTTPアクセスの終了
+http = 'GET /test.json HTTP/1.1\nHost: bokunimo.net\n\n'
 
+sock.send(http.encode())                        # HTTPリクエスト送信
+res = sock.recv(1024).decode()                  # HTTPレスポンス受信
+sock.close()                                    # HTTPアクセスの終了
+
+i_body = res.find('\r\n\r\n')
+if i_body < 0:
+    print(res)
+    exit()
+
+print(res[:i_body])
+print()
+res_dict = json.loads(res[i_body+4:])           # 受信データを変数res_dictへ代入
 print('title :', res_dict.get('title'))         # 項目'title'の内容を取得・表示
 print('descr :', res_dict.get('descr'))         # 項目'descr'の内容を取得・表示
 print('state :', res_dict.get('state'))         # 項目'state'の内容を取得・表示
