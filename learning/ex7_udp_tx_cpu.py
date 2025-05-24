8cd64926d0eda128ced39abf2d99c23cf03143ef
Wataru KUNINO <git@bokunimo.net>
2025-05-24 22:41:37 +0900

    ブロードIPの変数持

--- learning/ex7_udp_tx_cpu.py
+++ learning/ex7_udp_tx_cpu.py
@@ -8,13 +8,14 @@
 # pip install psutil
 
 # Windowsの場合はブロードキャストIPアドレスに255.255.255.255が使用できないので
-# sock.sendtoを同一セグメント内のブロードキャスト・IPアドレスに修正してください
-# (例えば192.168.1.0/24のセグメントの場合は、末尾255にした192.168.1.255宛)
+# brod_ipの値を同一セグメント内のブロードキャスト・IPアドレスに修正してください
+# (例えば192.168.1.0/24のセグメントの場合は、末尾255にした'192.168.1.255')
 
 import socket                                               # ソケットの組み込み
 import psutil                                               # システム情報取得
 from time import sleep                                      # スリープの組み込み
 
+brod_ip = '255.255.255.255'                                 # ブロードキャストIP
 port = 1024                                                 # ポート番号を代入
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)     # ソケットを作成
 sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST,1)   # ブロードキャスト
@@ -32,7 +33,7 @@ while True:                                                 # 繰り返し構文
     mem = round(info.get('memory').get('percent'), 0)       # メモリ使用量
     disk = round(info.get('disk_usage').get('percent'),0)   # ディスク使用量
     udp = 'cpu_m_1,'+str(cpu)+', '+str(mem)+', '+str(disk)+'\n' # 送信データ生成
-    sock.sendto(udp.encode(),('255.255.255.255',port))      # UDP送信
+    sock.sendto(udp.encode(),(brod_ip,port))                # UDP送信
     print('send :', udp, end='')                            # 送信データを出力
     sleep(10)                                               # 10秒の待ち時間処理
 sock.close()                                                # 切断(実行されない)
